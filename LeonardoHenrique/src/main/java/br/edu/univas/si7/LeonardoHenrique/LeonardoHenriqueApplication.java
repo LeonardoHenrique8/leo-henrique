83b6045aedc837c90e7a438202e8cbeb4735aabd
Leonardo Henrique <leownts@gmail.com>
2022-04-17 22:47:52 -0300

    first commit

---
+++ LeonardoHenrique/src/main/java/br/edu/univas/si7/LeonardoHenrique/LeonardoHenriqueApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univas.si7.LeonardoHenrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardoHenriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardoHenriqueApplication.class, args);
+	}
+
+}
---
+++ LeonardoHenrique/src/test/java/br/edu/univas/si7/LeonardoHenrique/LeonardoHenriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univas.si7.LeonardoHenrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonardoHenriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
